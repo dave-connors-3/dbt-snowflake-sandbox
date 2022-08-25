9b872873a31dc2efbf20ecb3f24c054dfa68911e
Dave Connors <dave.connors@fishtownanalytics.com>
2022-08-25 02:06:59 +0000

    integer surrogate key

---
+++ macros/integer_surrogate_key.sql
@@ -0,0 +1,18 @@
+{% macro integer_surrogate_key(field_list) %}
+    
+    {%- set fields = [] -%}
+    {%- for field in field list - %}
+    
+        {%- set _ = fields.append(
+        "coalesce(cast(" ~ field ~ " as varchar), '')"
+        )-%}
+
+    {%- if not loop.last %}
+        {%- set _ = fields.append("'-'") -%}
+    {%- endif -%}
+
+    {%- endfor -%}
+
+    md5_number_lower64({{ fields | join(' || ') }})
+    
+{%- endmacro -%}
