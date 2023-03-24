b7871bdf10e547067a11033940124b8663584e01
Dave Connors <dave.connors@fishtownanalytics.com>
2023-03-24 13:56:32 +0000

    rename

---
+++ models/sample_data.sql
@@ -0,0 +1,26 @@
+
+
+with data as (
+
+    select 1 as id
+
+    union all
+
+    
+
+    select 2 as id
+    
+    union all
+
+    select 3 as id
+    
+    union all
+
+    select 3 as id
+)
+
+select * from data
+
+
+-- {{ target.name in ['dev', 'default', 'ci'] }}
+-- {{ target.database }}
